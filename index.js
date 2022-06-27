c0b4de15e88d383f96957944983a994b9f9a5f4c
Santi Arroyo <santi.arroyo551@gmail.com>
2022-06-27 13:23:30 -0400

    commit tree c7275201c8f1d605a29912881ff41df3baa67d6f

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+
+//document.getElementById('text').innerText('This is really cool!')
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById('text').textContent = 'This is really cool!'
+  });
+
